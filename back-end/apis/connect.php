8ce1f9949a7a558aeaaa1e0a7fea29908e336ba1
josesanchezmx <montieljose711@gmail.com>
2024-04-30 19:50:30 -0600

    Modificacion de la base de datos por conflictos internos

--- back-end/apis/connect.php
+++ back-end/apis/connect.php
@@ -5,11 +5,10 @@ $password = "Admin12345#";
 $dbname = "iot_car";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
+  //echo "Connected successfully";
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-?>
